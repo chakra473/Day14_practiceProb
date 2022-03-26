e35c4d602f58011fb3c5a3d0a19358115f37ea63
chakravarthy <m.chakra473@gmail.com>
2022-03-26 21:57:10 +0530

    added file [Queue enqueue] to uc3

---
+++ QNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace QueueProgram
+{
+    internal class QNode
+    {
+
+        public int key;
+        public QNode next;
+
+        public QNode(int key)
+        {
+            this.key = key;
+            this.next = null;
+        }
+    }
+    class Queue
+    {
+        QNode front, rear;
+
+        public Queue()
+        {
+            this.front = this.rear = null;
+        }
+
+        public void enqueue(int key)
+        {
+            QNode temp = new QNode(key);
+
+            if (this.rear == null)
+            {
+                this.front = this.rear = temp;
+                return;
+            }
+            this.rear.next = temp;
+            this.rear = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Queue Problem");
+            Console.WriteLine("Adding elements to Queue");
+            Queue q = new Queue();
+            q.enqueue(56);
+            q.enqueue(30);
+            q.enqueue(70);
+            Console.WriteLine("statrting element is: " + q.front.key);
+
+        }
+    }
+}
