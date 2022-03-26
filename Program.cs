a282a314ee81e07e60b973ba852792c5d744ad22
chakravarthy <m.chakra473@gmail.com>
2022-03-26 21:47:06 +0530

    initiated

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day14_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stacks and queues!");
+        }
+    }
+}
